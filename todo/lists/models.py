3e6a7283ac82ac4cc311f3ec0d1f7eb98a46dd2d
swarna <starwars121012@gmail.com>
2020-05-27 14:55:30 +0530

    ignore test

--- todo/lists/models.py
+++ todo/lists/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     pass
+
+
 class TodoItem(models.Model):
     text = models.TextField(default='')
     user = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
