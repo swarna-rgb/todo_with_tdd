fbf1e22f07702ec54b5eff435c51a6cca96576f2
swarna <starwars121012@gmail.com>
2020-05-27 14:56:54 +0530

    gitignore test

--- todo/lists/models.py
+++ todo/lists/models.py
@@ -3,8 +3,6 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     pass
-
-
 class TodoItem(models.Model):
     text = models.TextField(default='')
     user = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
